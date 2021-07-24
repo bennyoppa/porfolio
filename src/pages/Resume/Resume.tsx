ede5b42f3b08e6b5d43df9e3db0d9aa2bbb32951
bennyoppa <benny.j.shi@gmail.com>
2021-07-25 00:10:29 +1000

    added 2 more pages

---
+++ src/pages/Resume/Resume.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import WithPage from "../../hocs/WithPage";
+
+const Resume = () => {
+  return (
+    <div>
+      <h1>Temp</h1>
+    </div>
+  );
+};
+
+export default WithPage(Resume);
